ec989055f505ce92dba9216ff2e4a9758fe265b7
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-12-03 13:02:42 -0700

    creates routes for models => controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+  post "/sessions" => "sessions#create"
+
+  get "/courses" => "courses#index"
+  get "/courses/:id" => "courses#show"
+  post "/courses" => "courses#create"
+  patch "/courses/:id" => "courses#update"
+  delete "courses/:id" => "courses#destroy"
+
+  get "/cards" => "cards#index"
+  get "/cards/:id" => "cards#show"
+  post "/cards" => "cards#create"
+  patch "/cards/:id" => "cards#update"
+  delete "cards/:id" => "cards#destroy"
+
+  get "/players" => "players#index"
+  get "/players/:id" => "players#show"
+  post "/players" => "players#create"
+  patch "/players/:id" => "players#update"
+  delete "players/:id" => "players#destroy"
 end
